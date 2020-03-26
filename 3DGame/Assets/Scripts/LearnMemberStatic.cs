0892ec58d751408aafbbc98e427f3dac119a56ce
KID <50666207+KID421@users.noreply.github.com>
2020-03-26 19:36:53 +0800

    認識程式基礎

---
+++ 3DGame/Assets/Scripts/LearnMemberStatic.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+public class LearnMemberStatic : MonoBehaviour
+{
+    // 定義屬性
+    // 語法：類型 名稱 指定 值;
+    float a = 0.5f;             // float 浮點數
+    int b = 70;                 // int 整數
+    bool c = true;              // bool 布林值
+    bool d = false;
+    string e = "我是字串";       // string 字串
+
+    private void Start()
+    {
+        // 使用靜態成員
+        // 靜態屬性 Static Properties
+        // 取得 Get：類別.靜態屬性
+        print(Random.value);
+        // 設定 Set：類別.靜態屬性 = 值
+        // 不能設定 Read Only 的屬性
+        Time.timeScale = 1f;
+    }
+}
