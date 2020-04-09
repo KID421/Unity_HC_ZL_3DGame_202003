91fdeb0e507de4916b600c48d3cefd0cc3538c18
KID <50666207+KID421@users.noreply.github.com>
2020-04-09 19:25:45 +0800

    載入畫面欄位

---
+++ 3DGame/Assets/Scripts/MenuManager.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using UnityEngine.UI;   // 引用 介面 API
+
+public class MenuManager : MonoBehaviour
+{
+    [Header("載入畫面")]
+    /// <summary>
+    ///  載入畫面
+    /// </summary>
+    public GameObject panelLoading;
+    [Header("載入畫面文字")]
+    /// <summary>
+    /// 載入畫面文字
+    /// </summary>
+    public Text textLoading;
+    [Header("載入畫面吧條")]
+    /// <summary>
+    /// 載入畫面吧條
+    /// </summary>
+    public Image imgLoading;
+
+    /// <summary>
+    /// 開始載入遊戲場景
+    /// </summary>
+    public void StartLoading()
+    {
+        print("開始載入...");
+    }
+}
