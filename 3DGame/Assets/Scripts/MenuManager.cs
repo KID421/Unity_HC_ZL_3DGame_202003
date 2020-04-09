37b77a8dcdc791bd1d827706f323fbd4872413c5
KID <50666207+KID421@users.noreply.github.com>
2020-04-09 21:35:28 +0800

    完成載入畫面

--- 3DGame/Assets/Scripts/MenuManager.cs
+++ 3DGame/Assets/Scripts/MenuManager.cs
@@ -1,5 +1,7 @@
 ﻿using UnityEngine;
-using UnityEngine.UI;   // 引用 介面 API
+using UnityEngine.UI;                   // 引用 介面 API
+using UnityEngine.SceneManagement;      // 引用 場景管理 API
+using System.Collections;               // 引用 系統.集合 API (協程) - 協同程式：非同步處理
 
 public class MenuManager : MonoBehaviour
 {
@@ -24,6 +26,36 @@ public class MenuManager : MonoBehaviour
     /// </summary>
     public void StartLoading()
     {
-        print("開始載入...");
+        panelLoading.SetActive(true);           // 顯示載入畫面
+
+        StartCoroutine(Loading());              // 啟動協程
+    }
+
+    /// <summary>
+    /// 協程方法：載入
+    /// </summary>
+    private IEnumerator Loading()
+    {
+        // SceneManager.LoadScene("關卡1");        // 載入場景
+        AsyncOperation ao = SceneManager.LoadSceneAsync("關卡1");
+
+        ao.allowSceneActivation = false;            // 關閉自動載入場景
+
+        // 迴圈 while(布林值) { 當布林值維 true 時執行敘述 }
+
+        while (ao.progress < 1)
+        {
+            textLoading.text = (ao.progress / 0.9f * 100).ToString("F2") + " %";    // 更新載入文字
+
+            imgLoading.fillAmount = ao.progress / 0.9f;                             // 更新載入吧條
+
+            yield return null;                   // 等待
+
+            // 判斷式 if(布林值) { 當布林值維 true 時執行一次敘述 }
+            if (ao.progress == 0.9f)                                                // 如果 進度 = 0.9
+            {
+                ao.allowSceneActivation = true;                                     // 允許自動載入場景
+            }
+        }
     }
 }
