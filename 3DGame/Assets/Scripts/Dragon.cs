632feaf2c0fda1dd3c9c2e47823d3d12a6aaf362
zxc333666 <62015305+zxc333666@users.noreply.github.com>
2020-04-23 21:30:54 +0800

    添加怪物

--- 3DGame/Assets/Scripts/Dragon.cs
+++ 3DGame/Assets/Scripts/Dragon.cs
@@ -15,6 +15,8 @@ public class Dragon : MonoBehaviour
     public GameObject fireBall;
     [Header("火球移動速度"), Range(1, 5000)]
     public float speedFireBall = 300;
+    [Header("攻擊力"), Range(1, 5000)]
+    public float attack = 35;
 
     // 第一種寫法：需要欄位
     // public Transform tra;
@@ -85,6 +87,9 @@ public class Dragon : MonoBehaviour
         posFire.y += 2.2f;
 
         GameObject temp = Instantiate(fireBall, posFire, Quaternion.identity);    // 生成(物件，座標，角度)
+
+        temp.AddComponent<Ball>();                  // 暫存火球.添加元件<球>()
+        temp.GetComponent<Ball>().damage = attack;  // 暫存火球.取得元件<球>().傷害值 = 攻擊力
 
         // Quaternion.identity Unity 角度類型 - 零角度
 
